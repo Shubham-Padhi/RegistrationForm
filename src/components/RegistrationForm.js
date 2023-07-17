c358055bcfb6ea548733f98e08b2d9dee46ef102
chiku <shubhampadhi404@gmail.com>
2023-07-17 22:30:18 +0530

    Registration-form-react-project

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import './RegistrationForm.css';
+
+const RegistrationForm = () => {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: ''
+  });
+  const [registrationSuccess, setRegistrationSuccess] = useState(false);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Registration form submitted:', formData);
+    setFormData({ firstName: '', lastName: '', email: '', password: '' });
+    setRegistrationSuccess(true);
+  };
+
+  return (
+    <div className="registration-container">
+      <h1>Registration Form</h1>
+      {registrationSuccess ? (
+        <div className="success-message">
+          Bhai tora  registration heigala...
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label>First Name:</label>
+            <input
+              type="text"
+              name="firstName"
+              value={formData.firstName}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label>Last Name:</label>
+            <input
+              type="text"
+              name="lastName"
+              value={formData.lastName}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label>Email:</label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label>Password:</label>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <button type="submit">Register</button>
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default RegistrationForm;
